7151fbfd61be8856399aac905f69d704455516f0
Matija Steblaj <matija.steblaj@gmail.com>
2016-10-31 14:28:35 +0100

    Dokoncal skripto za snemanje strani

--- Shrani strani.py
+++ Shrani strani.py
@@ -4,7 +4,7 @@ import requests
 import sys
 
 def naredi_mapo():
-    #Naredi mapo imdb v mapi eno stopnjo višje od trenutne poti
+    '''Naredi mapo imdb v mapi eno stopnjo višje od trenutne poti.'''
     trenutna_pot = os.getcwd()
     trenutna_mapa = trenutna_pot.split('\\')[-1]
     os.chdir('../')
@@ -12,7 +12,7 @@ def naredi_mapo():
     os.chdir(trenutna_mapa)
 
 def shrani_html(url, ime):
-    #V mapo imdb shrani tekst html strani iz url-ja url, v datoteko ime.txt
+    '''V mapo imdb shrani tekst html strani iz url-ja url, v datoteko ime.txt'''
     trenutna_pot = os.getcwd()
     trenutna_mapa = trenutna_pot.split('\\')[-1]
     os.chdir('../imdb')
@@ -21,10 +21,23 @@ def shrani_html(url, ime):
         f.write(str(r.text.encode('utf8')))
     os.chdir('../' + trenutna_mapa)
 
+
 def potegni_serije():
+    '''Iz datoteke list.txt iz mape imdb najde in shrani vseh 250 spletnih
+    strani serij v mapo imdb, pod njihovimi siframi, naredi legenda.txt,
+    kjer za vsako sifro pise, na katero serijo se nanasa.'''
     naredi_mapo()
-    shrani_html('http://www.imdb.com/chart/toptv/?sort=rk,asc&mode=simple&page=1',
+    shrani_html('http://www.imdb.com/chart/toptv/',
                 'list')
     os.chdir('../imdb')
     with open('list.txt', 'r') as f:
-        f.read()
+        text = f.read()
+    slovar_serij = {}
+    for stvar in re.finditer(r'.*?\d+\.\\n *?<a href="(/title/tt(\d+)/).*?>(.*?)</a>', text):
+        shrani_html('http://www.imdb.com' + stvar.group(1), stvar.group(2))
+        slovar_serij[stvar.group(2)] = stvar.group(3)
+    with open('legenda.txt', 'w') as g:
+        for sifra in slovar_serij:
+            g.write(sifra + ':' + slovar_serij[sifra] + ',')
+
+        
